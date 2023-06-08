1347abb4e681cf37a660791dc1360cbadf8e8b88
syckor <syckor@naver.com>
2023-06-08 22:41:32 +0900

    second day

--- src/main/java/com/example/core/order/OrderServiceImpl.java
+++ src/main/java/com/example/core/order/OrderServiceImpl.java
@@ -2,6 +2,7 @@ package com.example.core.order;
 
 import com.example.core.discount.DiscountPolicy;
 import com.example.core.discount.FixDiscountPolicy;
+import com.example.core.discount.RateDiscountPolicy;
 import com.example.core.member.Member;
 import com.example.core.member.MemberRepository;
 import com.example.core.member.MemoryMemberRepository;
@@ -9,7 +10,9 @@ import com.example.core.member.MemoryMemberRepository;
 public class OrderServiceImpl implements OrderService {
 
     private final MemberRepository memberRepository = new MemoryMemberRepository();
-    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+    //private final DiscountPolicy discountPolicy = new FixDiscountPolicy(); //DIP원칙 위배
+    private  DiscountPolicy discountPolicy = new RateDiscountPolicy(); //OCP원칙 위배
+
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
