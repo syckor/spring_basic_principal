f5be600507ee9a9482532dbe2beb7494565c2dea
syckor <syckor@naver.com>
2023-06-10 23:29:48 +0900

    3rd day 스프링으로 변경

--- src/main/java/com/example/core/AppConfig.java
+++ src/main/java/com/example/core/AppConfig.java
@@ -9,20 +9,28 @@ import com.example.core.member.MemberServiceImpl;
 import com.example.core.member.MemoryMemberRepository;
 import com.example.core.order.OrderService;
 import com.example.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class AppConfig {
 
+    @Bean
     public MemberService memberService(){
         return new MemberServiceImpl(memberRepository());
     } //생성자 주입
 
+
     private MemberRepository memberRepository(){
         return new MemoryMemberRepository();
     }
+
+    @Bean
     public OrderService orderService(){
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
+    @Bean
     public DiscountPolicy discountPolicy(){
         /*정액할인에서 정률할인정책으로 바뀐다면 이 부분의 return값만 바꾸면 된다.
         * */
--- src/main/java/com/example/core/OrderApp.java
+++ src/main/java/com/example/core/OrderApp.java
@@ -7,16 +7,22 @@ import com.example.core.member.MemberServiceImpl;
 import com.example.core.order.Order;
 import com.example.core.order.OrderService;
 import com.example.core.order.OrderServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class OrderApp {
 
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
-        OrderService orderService = appConfig.orderService();
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = appConfig.memberService();
+//        OrderService orderService = appConfig.orderService();
 
 //        MemberService memberService = new MemberServiceImpl();
 //        OrderService orderService = new OrderServiceImpl();
+
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = ac.getBean("memberService", MemberService.class);
+        OrderService orderService = ac.getBean("orderService", OrderService.class);
 
         Long memberId = 1L;
         Member member = new Member(memberId, "memberA", Grade.VIP);
