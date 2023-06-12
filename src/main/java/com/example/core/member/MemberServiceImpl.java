34475bca96930372fe0ac6b9adcd24f08697e7f4
syckor <syckor@naver.com>
2023-06-12 22:30:18 +0900

    component와 생성자 주입 방법

--- src/main/java/com/example/core/member/MemberServiceImpl.java
+++ src/main/java/com/example/core/member/MemberServiceImpl.java
@@ -1,9 +1,15 @@
 package com.example.core.member;
 
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class MemberServiceImpl implements MemberService{
 
     private final MemberRepository memberRepository; //추상화에만 의존하고 구체 클래스는 몰라도 된다. dip 원칙 지킴
 
+    @Autowired //memberRepository에 맞는 구현체를 자동으로 주입
     public MemberServiceImpl(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
     }
--- src/main/java/com/example/core/order/OrderServiceImpl.java
+++ src/main/java/com/example/core/order/OrderServiceImpl.java
@@ -6,15 +6,31 @@ import com.example.core.discount.RateDiscountPolicy;
 import com.example.core.member.Member;
 import com.example.core.member.MemberRepository;
 import com.example.core.member.MemoryMemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
+@Component
 public class OrderServiceImpl implements OrderService {
 
-    private final MemberRepository memberRepository;
-    private final DiscountPolicy discountPolicy;
+//    @Autowired private MemberRepository memberRepository;필드 주입(권장하지 않음)
+//    @Autowired private DiscountPolicy discountPolicy;     -> 외부에서 테스트하기 힘듬, 결국 setter를 만들어야함..
+
+    private MemberRepository memberRepository;//final은 값이 항상 있어야 함
+    private DiscountPolicy discountPolicy;
     //private final DiscountPolicy discountPolicy = new FixDiscountPolicy(); //DIP원칙 위배
     //private  DiscountPolicy discountPolicy = new RateDiscountPolicy(); //OCP원칙 위배
 
+    //수정자 주입
+    @Autowired
+    public void setMemberRepository(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+    @Autowired
+    public void setDiscountPolicy(DiscountPolicy discountPolicy) {
+        this.discountPolicy = discountPolicy;
+    }
 
+    //생성자가 하나 있으면 Autowired를 안붙여도 된다.
     public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
